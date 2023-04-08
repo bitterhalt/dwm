8896aeb77325179999595aa25bad0036be6b9eda
bitterhalt <95308907+bitterhalt@users.noreply.github.com>
2023-04-08 21:57:40 +0300

    yadayadyad

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
@@ -103,20 +103,22 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browser[] = { "firefox", NULL };
+static const char *rofidmenu[]  = { "rofidmenu", NULL };
+static const char *powermenu[] = { "powermenu", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-    { MODKEY|ShiftMask,              XK_w,      spawn,          {.v = browser } },
+    { MODKEY|ShiftMask,             XK_w,      spawn,          {.v = browser } },
+    { MODKEY,                       XK_d,      spawn,          {.v = rofidmenu } },
+    { MODKEY|ShiftMask,             XK_p,      spawn,          {.v = powermenu } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_Prior,  incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_Next,   incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-    { MODKEY|ShiftMask,             XK_up,      movestack,      {.i = +0 } },
-	{ MODKEY|ShiftMask,             XK_down,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_u,      incrgaps,       {.i = +5 } },
 	{ MODKEY|ShiftMask,             XK_u,      incrgaps,       {.i = -5 } },
 	{ MODKEY,                       XK_i,      incrigaps,      {.i = +5 } },
